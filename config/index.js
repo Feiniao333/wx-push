da4d912a056c9ec0b066457a9e83cbf58cd6a31f
Feiniao333 <3359493411@qq.com>
2025-02-15 20:10:10 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxfba26ac846d53362",
+  appsecret: "37853ce52b1fd9291941bf7ea425643a",
+  template_id: "UIqcWW-PWdTPyTt2vo-tDUgE1i4_FnauHkKroTwrTbY",
+  touser: "opTdR7Mukd-7BkNhqlr_5J2O1Qjw",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "重庆",
+  first_date: "2023-7-1",
 };
